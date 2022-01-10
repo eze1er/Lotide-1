deb4bfa2149855347629c537ed0e7c0c7c477fbf
eze1er <papaezechiel@gmail.com>
2022-01-10 23:14:12 +0000

    Commit after modification for to check element in arrays

---
+++ assertEqual.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return true;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return true;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  return false;
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const cd = ["2", 3];
+const dc = ["2", 3];
+assertEqual(cd, dc); // => true
+
+const cd2 = ["2", 3, 4];
+assertEqual(cd, cd2); // => false
