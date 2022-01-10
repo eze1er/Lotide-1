de0d509557196a78ea0481c487e0f9a1e7f5e9de
eze1er <papaezechiel@gmail.com>
2022-01-10 23:55:07 +0000

    chek undefined added

--- assertEqual.js
+++ assertEqual.js
@@ -1,19 +1,23 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   const inspect = require('util').inspect;
+  if (actual === undefined) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
+  }
   if (actual.length !== expected.length) {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-    return true;
+    return false;
   }
 
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-      return true;
+      return false;
     }
   }
   console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  return false;
+  return true;
 };
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
@@ -22,6 +26,8 @@ assertEqual(1, 1);
 const cd = ["2", 3];
 const dc = ["2", 3];
 assertEqual(cd, dc); // => true
-
+const cd1 = undefined;
+const dc1 = ["2", 3];
+assertEqual(cd1, dc1); // => true
 const cd2 = ["2", 3, 4];
 assertEqual(cd, cd2); // => false
