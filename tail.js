f08e2094c68f1220b92b2f96813e3d5b1f646b7b
eze1er <papaezechiel@gmail.com>
2022-01-11 00:19:04 +0000

    First commit

---
+++ tail.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if ((actual === undefined) && (expected === undefined)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
+  }
+  if ((actual === undefined) || (expected === undefined) || (actual.length !== expected.length))  {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  return true;
+};
+
+const tail = function(array) {
+  if (array.length === 0 || array.length === 1) {
+    return [];
+  }
+  let array1 = [];
+  for (let i = 1; i < array.length; i++) {
+    array1.push(array[i]);
+  }
+  return array1;
+};
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
