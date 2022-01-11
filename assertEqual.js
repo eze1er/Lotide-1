dd2ec057c0ccac2e5bc38ee4702eaa0b54b38c4e
eze1er <papaezechiel@gmail.com>
2022-01-11 00:08:57 +0000

    assertEqual improve for undefined array

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  if ((actual === undefined) || (actual.length !== expected.length))  {
+  if ((actual === undefined) && (expected === undefined)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
+  }
+  if ((actual === undefined) || (expected === undefined) || (actual.length !== expected.length))  {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
     return false;
   }
--- head.js
+++ head.js
@@ -3,21 +3,20 @@ const assertEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if ((actual === undefined) && (expected === undefined)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-    return false;
-  }
-  if (actual.length !== expected.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
     return true;
   }
-
+  if ((actual === undefined) || (expected === undefined) || (actual.length !== expected.length))  {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
+  }
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
       console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
-      return true;
+      return false;
     }
   }
   console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
-  return false;
+  return true;
 };
 const head = function(array) {
   if (array.length === 0) {
@@ -32,5 +31,5 @@ const head = function(array) {
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+assertEqual(head([]), []);
 assertEqual(head([6]), 6);
