caa48a35e1280b1c54ca8807fd538919dfaabd90
eze1er <papaezechiel@gmail.com>
2022-01-12 19:35:14 +0000

    First commit

---
+++ countOnly.js
@@ -0,0 +1,56 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if ((actual === undefined) && (expected === undefined)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
+  }
+  if ((actual === undefined) || (expected === undefined) || (actual.length !== expected.length))  {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  return true;
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
