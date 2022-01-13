63794d5a95bff5f28dfc07b22ba986057daf27d3
eze1er <papaezechiel@gmail.com>
2022-01-13 20:39:53 +0000

    First commit

---
+++ map.js
@@ -0,0 +1,47 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  // empty for now :)
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+
+// test functions
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const inspect = require('util').inspect;
+  if ((array1 === undefined) && (array2 === undefined)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`);
+    return true;
+  }
+  // console.log(array1, array2);
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} !== ${inspect(array2)}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${inspect(array1)} === ${inspect(array2)}`);
+  return true;
+};
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); // => should PASS
+
