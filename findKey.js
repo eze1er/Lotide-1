25724eba2f30ee3b45f2910d1de53afac6341070
eze1er <papaezechiel@gmail.com>
2022-01-13 22:10:02 +0000

    First commit

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if ((actual === undefined) && (expected === undefined)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    return true;
+  }
+  if ((actual === undefined) || (expected === undefined) || (actual.length !== expected.length))  {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return false;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  return true;
+};
+const findKey = function(array, callback) {
+  for (const value in array) {
+    if (callback(array[value])) {
+      console.log(`expected: ${value}`);
+      return value;
+
+    }
+  }
+
+};
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'); // => "noma"
+
