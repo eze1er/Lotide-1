270f0f1d1398855d3f1969571bac87701de62055
eze1er <papaezechiel@gmail.com>
2022-01-17 23:45:13 +0000

    commit all function to index.js

--- tail.js
+++ tail.js
@@ -3,7 +3,5 @@ const assertEqual = require('./assertEqual');
 const tail = function(array) {
   return array.slice(1);
 };
-const words = ["Hello", "Lighthouse", "Labs"];
-console.log(tail(words));
 
 module.exports = tail;
